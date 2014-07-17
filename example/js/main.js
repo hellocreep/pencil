61ff708849f1e188c1438300a8affa1fd8244024
hellocreep <hellocreep@gmail.com>
2014-07-17 20:37:49 +0800

    Updates

--- example/js/main.js
+++ example/js/main.js
@@ -1,7 +1,16 @@
 (function() {
 $(function() {
+  var $win = $(window);
+
+  $('#canvas').attr({
+    width: $win.width(),
+    height: $win.height()
+  });
+
   pencil = new Pencil();
   window.pencil = pencil;
+
+
 
   $('.js-pencil-play').on('click', function() {
     pencil.play();
