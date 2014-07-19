00505da36e95898da4d33552bc94f02832ab0864
hellocreep <hellocreep@gmail.com>
2014-07-19 14:46:27 +0800

    Updates

--- example/js/main.js
+++ example/js/main.js
@@ -4,10 +4,12 @@ $(function() {
 
   $('#canvas').attr({
     width: $win.width(),
-    height: $win.height()
+    height: $win.height() - $('#header').height() - $('#footer').height()
   });
 
-  pencil = new Pencil();
+  $('#wrap').height($win.height())
+
+  pencil = new Pencil('canvas');
   window.pencil = pencil;
 
   $('.js-set').on('click', function() {
@@ -16,6 +18,21 @@ $(function() {
 
   $('.js-pencil-play').on('click', function() {
     pencil.play();
+    $('.js-toggle-box').trigger('click');
+  });
+
+  $('.js-toggle-box').on('click', function() {
+    var $this = $(this);
+    var anime;
+    var top = $('.pencilbox').height();
+    if($this.hasClass('down')) {
+      anime = {top: 0};
+    } else {
+      anime = {top: -top}
+    }
+    $(this).parent().animate(anime, 300, function() {
+      $this.toggleClass('down');
+    });
   });
 
 
